f304868acb9836f0989fbd1dfecacf1935d44f07
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2022-09-16 23:23:06 +0430

    final

--- src/App.js
+++ src/App.js
@@ -6,13 +6,9 @@ import CountriesContextProvider from './context/CountriesContextProvider';
 // *router
 import Routers from './Routers';
 
-// *components
-import Navbar from './components/Navbar';
-
 const App = () => {
   return (
     <div>
-      <Navbar />
       <CountriesContextProvider>
         <div className='w-11/12 m-auto mt-6'>
           <Routers />
